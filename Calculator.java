a672d4093bc1cee79d8b6f0f15f1d1c6affdb607
ellidelli <elizabeth.delea@gmail.com>
2023-02-07 19:43:56 -0500

    added multiplication and division options

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        String thing = null;
         int ans = 0;
         Scanner scan = new Scanner(System.in);
 
@@ -10,16 +9,20 @@ public class Calculator {
         int first = scan.nextInt();
         int second = scan.nextInt();
 
-        System.out.println("Would you like to:" + "\n1. Add\n2. Subtract");
+        System.out.println("Would you like to:" + "\n1. Add\n2. Subtract\n3.Multiply\n4.Divide");
         int choice = scan.nextInt();
         if (choice == 1) {
             ans = first + second;
-            thing = " + ";
-            System.out.println(first + thing + second + " = " + ans);
+            System.out.println(first + " + " + second + " = " + ans);
         } else if (choice == 2) {
             ans = first - second;
-            thing = " - ";
-            System.out.println(first + thing + second + " = " + ans);
+            System.out.println(first + " - " + second + " = " + ans);
+        } else if (choice == 3) {
+            ans = first * second;
+            System.out.println(first + " * " + second + " = " + ans);
+        } else if (choice == 4) {
+            ans = first / second;
+            System.out.println(first + " / " + second + " = " + ans);
         } else {
             System.out.println("Invalid input");
         }
