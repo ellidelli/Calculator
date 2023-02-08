59286c5da4e5b9958d588823b45fdced4aa728b2
ellidelli <elizabeth.delea@gmail.com>
2023-02-08 10:54:17 -0500

    added a divide by 0 error

--- Calculator.java
+++ Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
         float first = scan.nextFloat();
         float second = scan.nextFloat();
 
-        System.out.println("Would you like to:" + "\n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+        System.out.println("\nWould you like to:" + "\n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
         int choice = scan.nextInt();
         if (choice == 1) {
             ans = first + second;
@@ -21,8 +21,12 @@ public class Calculator {
             ans = first * second;
             System.out.println(first + " * " + second + " = " + ans);
         } else if (choice == 4) {
+            if (second == 0) {
+                System.out.println("Error: divide by zero.");
+            } else {
             ans = first / second;
             System.out.println(first + " / " + second + " = " + ans);
+            }
         } else {
             System.out.println("Invalid input");
         }
