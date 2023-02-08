a34d64b421a92f50d673c4f03f38cbab1587a7ce
ellidelli <elizabeth.delea@gmail.com>
2023-02-07 19:40:43 -0500

    added ability to choose between addition or subtraction

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        String thing = null;
+        int ans = 0;
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter two numbers.");
         int first = scan.nextInt();
         int second = scan.nextInt();
-        int sum = first + second;
-        System.out.println(first + " + " + second + " = " + sum);
+
+        System.out.println("Would you like to:" + "\n1. Add\n2. Subtract");
+        int choice = scan.nextInt();
+        if (choice == 1) {
+            ans = first + second;
+            thing = " + ";
+            System.out.println(first + thing + second + " = " + ans);
+        } else if (choice == 2) {
+            ans = first - second;
+            thing = " - ";
+            System.out.println(first + thing + second + " = " + ans);
+        } else {
+            System.out.println("Invalid input");
+        }
     }
 }
